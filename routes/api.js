4bb13091dbf411e1a82aca84a5e1760dccd157de
Glitch (fcc-messageboard-gmm) <none>
2020-03-10 01:38:27 +0000

    🌠🌷 Checkpoint ./routes/api.js:706117/737

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,28 @@
 'use strict';
 
 var expect = require('chai').expect;
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+const MONGODB_CONNECTION_STRING = process.env.DB;
 
 module.exports = function (app) {
+  let boardsCollection;
+  let repliesCollection;
+  let threadsCollection;
+  
+  //initialize the database connection
+  const client = new MongoClient(MONGODB_CONNECTION_STRING, { useNewUrlParser: true });
+  client.connect(err => {
+    if(err) throw err;
+    boardsCollection = client.db("messageboard").collection("boards");
+    repliesCollection = client.db("messageboard").collection("replies");
+    threadsCollection = client.db("messageboard").collection("threads");
+    console.log('DB Connected');
+  });
+
   
   app.route('/api/threads/:board').post((req,res)=>{
-    console.log(req.query);
+    console.log(req.body);
   });
     
   app.route('/api/replies/:board');
